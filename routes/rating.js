a9c25dacc1f0d3ccfe6da88efa503ac499f31514
2845814211@qq.com <2845814211@qq.com>
2025-04-02 13:23:33 +0800

    修复控制器导入路径大小写问题

--- routes/rating.js
+++ routes/rating.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const ratingController = require("../controllers/RatingController");
-const { verifyTokenAndAuthorization } = require("../middleware/verifyToken")
+const ratingController = require("../controllers/ratingController");
+const { verifyTokenAndAuthorization } = require("../middleware/verifyToken");
 
 router.post("/", verifyTokenAndAuthorization, ratingController.addRating);
 
 router.get("/", verifyTokenAndAuthorization, ratingController.checkUserRating);
 
-module.exports = router
+module.exports = router;
